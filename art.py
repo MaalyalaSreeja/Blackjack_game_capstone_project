32af15beb2f3893b82dbfa80f88d59e79e398515
maalyala Sreeja <sreejamalyala497@gmail.com>
2024-04-11 14:22:50 +0530

    Implemented basic functionality for the console-based blackjack game
    This project aims to create a console-based game where players can play blackjack against a computer dealer. The game will feature standard blackjack  rules, including player betting, hitting, standing, and dealer actions.

---
+++ art.py
@@ -0,0 +1,12 @@
+# Blackjack_game_capstone_project
+ #This project aims to create a console-based game where players can play blackjack aganist a computer dealer. The game will feature standard blackjack  rules, including player betting, hitting, standing, and dealer actions.
+logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
